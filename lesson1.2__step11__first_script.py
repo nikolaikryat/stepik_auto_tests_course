5e9744535cdc8356007ef246b0a8f71728d1bf23
Niko Kryat <nikolai.kryat@mail.ru>
2025-06-01 14:01:18 +0300

    add_first_script

---
+++ lesson1.2__step11__first_script.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://suninjuly.github.io/text_input_task.html")
+
+textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+textarea.send_keys("get()")
+
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+submit_button.click()
+time.sleep(5)
+
+driver.quit()
