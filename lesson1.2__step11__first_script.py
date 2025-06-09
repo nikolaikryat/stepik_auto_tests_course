0daca40b3960810b6e499f027af804b2a2f39b55
Niko Kryat <nikolai.kryat@mail.ru>
2025-06-09 14:14:56 +0300

    add try/finally construction

--- lesson1.2__step11__first_script.py
+++ lesson1.2__step11__first_script.py
@@ -2,14 +2,17 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+link = "https://suninjuly.github.io/text_input_task.html"
 driver = webdriver.Chrome()
-driver.get("https://suninjuly.github.io/text_input_task.html")
 
-textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
-textarea.send_keys("get()")
+try:
+    driver.get(link)
 
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
-submit_button.click()
-time.sleep(5)
+    textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+    textarea.send_keys("get()")
 
-driver.quit()
+    submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+    submit_button.click()
+    time.sleep(5)
+finally:
+    driver.quit()
