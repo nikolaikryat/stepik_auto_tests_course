3e72deaea0330463186fe01751cdd94ca96cf3e0
Niko Kryat <nikolai.kryat@mail.ru>
2025-07-04 02:36:54 +0300

    add_broken_fixture_scripts

--- pytest+fixtures/test_lesson1_2_step11__first_script.py
+++ pytest+fixtures/test_lesson1_2_step11__first_script.py
@@ -4,27 +4,18 @@ import pytest
 
 link = "https://suninjuly.github.io/text_input_task.html"
 
-@pytest.fixture
-def browser():
-    print("\nstart driver for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    # этот код выполнится после завершения теста
-    print("\nquit browser..")
-    browser.quit()
 class TestMainPage():
 
     def setup_method(self):
-        print("start browser for test..")
+        print("\nstart browser for test..")
         self.browser = webdriver.Chrome()
 
     def teardown_method(self):
-        print("quit browser for test..")
+        print("\nquit browser for test..")
         self.browser.quit()
 
     def test_get_rest(self):
-    self.browser.get(link)
-    self.browser.implicitly_wait(5)
-
-    self.browser.find_element(By.CSS_SELECTOR, ".textarea").send_keys("get()")
-    self.driver.find_element(By.CSS_SELECTOR, ".submit-submission").click()
+        self.browser.get(link)
+        self.browser.implicitly_wait(5)
+        self.browser.find_element(By.CSS_SELECTOR, ".textarea").send_keys("get()")
+        self.browser.find_element(By.CSS_SELECTOR, ".submit-submission").click()
